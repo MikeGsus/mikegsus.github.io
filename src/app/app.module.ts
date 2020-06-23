de3b7d5fa6dec7262e31203ef0fd6a41e272fbd0
MikeGsus <mike08ahj@hotmail.com>
2020-06-23 03:41:48 -0500

    Fix navbar and container styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ContainerComponent } from './container/container.component';
+import { NavbarComponentComponent } from './navbar-component/navbar-component.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContainerComponent
+    NavbarComponentComponent
   ],
   imports: [
     BrowserModule,
