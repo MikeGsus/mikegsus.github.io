cd8185cf11eecd0e5f27d7a564125f6139ebbdc2
MikeGsus <mike08ahj@hotmail.com>
2020-06-23 15:34:47 -0500

    Import icons

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import {
+  faFacebookF,
+  faTwitter,
+  faLinkedinIn,
+  faInstagram
+} from '@fortawesome/free-brands-svg-icons'
 
 @Component({
   selector: 'app-footer',
@@ -6,6 +12,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
+
+  facebook = faFacebookF
+  twitter = faTwitter
+  linkedin = faLinkedinIn
+  instagram = faInstagram
 
   constructor() { }
 
