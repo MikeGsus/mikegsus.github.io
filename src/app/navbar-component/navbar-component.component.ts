3eda9669ceefe0f7714420952e6ddf824aee694e
MikeGsus <mike08ahj@hotmail.com>
2020-07-16 18:03:44 -0500

    Add action to set smaller the navbar when scroll

--- src/app/navbar-component/navbar-component.component.ts
+++ src/app/navbar-component/navbar-component.component.ts
@@ -10,6 +10,16 @@ export class NavbarComponentComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    window.onscroll = () => { this.scrollFunction() }
   }
 
+  scrollFunction () {
+    if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+      document.getElementById("navbar-top").style.padding = "1rem 10px";
+      document.getElementById("title").style.fontSize = "20px";
+    } else {
+      document.getElementById("navbar-top").style.padding = "1.5rem 10px";
+      document.getElementById("title").style.fontSize = "35px";
+    }
+  }
 }
