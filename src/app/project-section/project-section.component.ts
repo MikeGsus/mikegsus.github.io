e17be8b65fa369aa5454f371fa4541711f04e97a
MikeGsus <mike08ahj@hotmail.com>
2020-08-05 22:03:15 -0500

    Add projects array

--- src/app/project-section/project-section.component.ts
+++ src/app/project-section/project-section.component.ts
@@ -7,7 +7,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectSectionComponent implements OnInit {
 
-  constructor() { }
+  base = '../../assets/img'
+  projects = [
+    {
+      name: 'Fin360',
+      url: 'https://www.fin360.mx',
+      src: `${this.base}/svg/fin360.svg`,
+      description: ''
+    },
+    {
+      name: 'Letica',
+      url: 'https://play.google.com/store/apps/details?id=com.softcontrolsinternational.letica',
+      src: `${this.base}/svg/letica.svg`,
+      description: ''
+    },
+    {
+      name: 'Universidades Renovadas',
+      url: 'https://play.google.com/store/apps/details?id=com.renovaciontabasco.gouapp',
+      src: `${this.base}/webp/gou.webp`,
+      description: ''
+    },
+    {
+      name: 'Curia Diocesana',
+      url: '',
+      src: `${this.base}/png/curia.png`,
+      description: ''
+    }
+  ]
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
