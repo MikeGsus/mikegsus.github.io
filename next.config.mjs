cd9711165d22b146640ce08a65fd0b358186a8dc
MikeGsus <mike08ahj@hotmail.com>
2025-08-01 15:02:18 -0600

    feat: update ESLint configuration, add TypeScript support, and create initial configuration files

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,20 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'blob.v0.dev',
+      },
+    ],
+    unoptimized: true,
+  },
+};
 
 export default nextConfig;
